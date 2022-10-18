d7da480129460e657d14f09abf7f2d43c6d720a3
kushal-kamra <kushalkamradev@gmail.com>
2022-10-18 03:56:50 +0000

    User Input

--- java/StockExchange.java
+++ java/StockExchange.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 class StockExchange {
     public static void main(String args[]) {
-        System.out.println("Hello Worlds!");
+        System.out.println("Stock Exchange Online");
+        Scanner input = new Scanner(System.in);
+
+        while(true) {
+            System.out.println("Enter a new order or type 'bye' to Exit: ");
+            String line = input.nextLine();
+
+            if ("bye".equalsIgnoreCase(line)) {
+                break;
+            }
+            
+            System.out.println("Order received: " + line);
+        }
+
+        System.out.println("Exiting Stock Exchange");
     }
 }
